d48405fd17e6582de86a8b2c1b1c53468afa6f66
injang12 <alsrb977@gmail.com>
2025-04-03 09:34:41 +0900

    Final

--- VisionSystem/Program.cs
+++ VisionSystem/Program.cs
@@ -3,11 +3,6 @@
     internal static class Program
     {
         [System.STAThread]
-        static void Main()
-        {
-            System.Windows.Forms.Application.EnableVisualStyles();
-            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
-            System.Windows.Forms.Application.Run(new MainForm());
-        }
+        static void Main() => System.Windows.Forms.Application.Run(new MainForm());
     }
 }
